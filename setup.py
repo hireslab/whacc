06b936aeb13b166e736188655c57ce2af45a79e3
PhillipMaire <phillip.maire@gmail.com>
2021-04-10 16:15:45 -0700

    updated read me and setup file
    updated read me and setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
 from setuptools import setup
 from setuptools import find_packages
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='whacc',
-      version='0.0.11',
+      version='0.0.13',
+      author="Phillip Maire",
+      license='MIT',
       description='largely automatic and customizable pipeline for creating a CNN to predict whiskers contacting objects',
       packages=find_packages(),
       author_email='phillip.maire@gmail.com',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       zip_safe=False,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
+      python_requires='>=3.6',
       install_requires=[
        # "pyicu",
        "natsort==7.1.1"])
+
+# package_data={
+    #     'whacc': ['datasets/*.h5',
+    #               'model_checkpoints/*',
+    #               'model_checkpoints/model_201008/*',
+    #               'model_checkpoints/model_201008/variables/*',
+    #               'model_checkpoints/model_201008/assets/*'],
+    # },
+# include_package_data=True,
+
+
+
