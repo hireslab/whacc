a545cf2c5e25bd5d22344e25133aec82cc2ba8a9
PhillipMaire <phillip.maire@gmail.com>
2021-05-03 09:01:00 -0700

    removed auto add pole down times to subset generator
    removed auto add pole down times to subset generator

--- whacc/examples/gui_example.py
+++ whacc/examples/gui_example.py
@@ -1,7 +1,7 @@
-H5_file_name = '/Users/phil/Dropbox/Autocurator/data/samsons_subsets/train_and_validate/AH1131X25032020ses335_subset.h5'
+H5_file_name = '/Users/phil/Dropbox/Autocurator/data/samsons_subsets/training_and_validation/AH1131X25032020ses335_subset.h5'
+H5_file_name = '/Users/phil/Dropbox/Autocurator/data/samsons_subsets/training_and_validation/AH1131X26032020ses338_subset.h5'
 label_read_key = 'labels'
 label_write_key = 'labels'
 from whacc.touch_curation_GUI import touch_gui
 touch_gui(H5_file_name, label_read_key, label_write_key)
-
 
