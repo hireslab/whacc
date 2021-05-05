478291047b376b7a999693fa8a38a75431d28b5f
PhillipMaire <phillip.maire@gmail.com>
2021-05-04 23:44:15 -0700

    changed the augment H5 example file
    changed the augment H5 example file to include gaussian noise that is actually helpful when training the program

--- whacc/examples/gui_example.py
+++ whacc/examples/gui_example.py
@@ -1,5 +1,5 @@
-H5_file_name = '/Users/phil/Dropbox/Autocurator/data/samsons_subsets/training_and_validation/AH1131X25032020ses335_subset.h5'
-H5_file_name = '/Users/phil/Dropbox/Autocurator/data/samsons_subsets/training_and_validation/AH1131X26032020ses338_subset.h5'
+# H5_file_name = '/Users/phil/Dropbox/Autocurator/data/samsons_subsets/training_and_validation/AH1131X25032020ses335_subset.h5'
+H5_file_name = '/Users/phil/Dropbox/Autocurator/data/samsons_subsets/OLD_2/AH1159X27012021xS414_subset.h5'
 label_read_key = 'labels'
 label_write_key = 'labels'
 from whacc.touch_curation_GUI import touch_gui
--- whacc/examples/scratch_2.py
+++ whacc/examples/scratch_2.py
@@ -1,10 +1,38 @@
 import h5py
 import matplotlib.pyplot as plt
 from whacc import image_tools
+import os
+from whacc import utils
+
+for h5_file in utils.get_h5s('/Users/phil/Dropbox/Autocurator/data/samsons_subsets/OLD_2'):
+    with h5py.File(h5_file, 'r') as h:
+        plt.figure(figsize=[20, 10])
+        plt.imshow(image_tools.img_unstacker(h['images'][:],40))
+        plt.title(h5_file.split('/')[-1])
+        plt.savefig(h5_file[:-3] + "_fig_plot.png")
+        plt.close()
 
 
-h5_file = "/Users/phil/Dropbox/Autocurator/data/samsons_subsets/test/AH1159X27012021xS414_subset_2.h5"
+for k in utils.get_h5s('/Users/phil/Dropbox/Autocurator/data/samsons_subsets/OLD_2'):
+    with h5py.File(k, 'r') as h:
+        k2 = k[:-3]+'_TMP.h5'
+        with h5py.File(k2, 'w') as h2:
+            h2.create_dataset('all_inds', data=h['all_inds'][50:])
+            h2.create_dataset('images', data=h['images'][50:])
+            h2.create_dataset('in_range', data=h['in_range'][50:])
+            h2.create_dataset('labels', data=h['labels'][50:])
+            h2.create_dataset('retrain_H5_info', data=h['retrain_H5_info'][:])
+    os.remove(k)
+    os.rename(k2, k)
+
+
+import h5py
+import matplotlib.pyplot as plt
+from whacc import image_tools
+
+h5_file= '/Users/phil/Downloads/AH1159X18012021xS404_subset (1).h5'
 with h5py.File(h5_file, 'r') as h:
-    plt.imshow(image_tools.img_unstacker(h['images'][:], 21))
-
-
+    plt.figure(figsize=[20, 10])
+    plt.imshow(image_tools.img_unstacker(h['images'][:],10))
+    plt.title(h5_file.split('/')[-1])
+    print(len(h['labels'][:]))
