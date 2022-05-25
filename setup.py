bc2242a7f81c2672c5716740f419e752deeb9d39
PhillipMaire <phillip.maire@gmail.com>
2022-05-25 14:47:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,22 @@ from setuptools import find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+import os
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            if filename[0] != '.':
+                paths.append(os.path.join('..', path, filename))
+    return paths
+
+extra_files = package_files('/Users/phil/Dropbox/HIRES_LAB/GitHub/whacc/whacc/whacc_data')
+extra_files
+
 setup(name='whacc',
-      version='1.1.20',
+      version='1.1.25',
       author="Phillip Maire",
       license='MIT',
       description='Automatic and customizable pipeline for creating a CNN + light GBM model to predict whiskers contacting objects',
@@ -20,16 +34,18 @@ setup(name='whacc',
                    "Operating System :: OS Independent"],
       python_requires='>=3.6',
       install_requires=["natsort==7.1.1"],
-      package_data={'whacc': ['whacc_data/*',
-                              'whacc/whacc_data/feature_data/*.pkl',
-                              'whacc/whacc_data/*.png',
-                              'whacc_data/final_model/final_resnet50V2_full_model/variables/*',
-                              'whacc_data/final_model/final_resnet50V2_full_model/assets/*',
-                              'whacc_data/final_model/final_resnet50V2_full_model/*',
-                              'whacc_data/final_model/*',
-                              'whacc/whacc_data/final_model/final_resnet50V2_full_model/*pb',
-                              'whacc/whacc_data/final_model/final_resnet50V2_full_model/variables/*.index',
-                              'whacc/whacc_data/final_model/final_resnet50V2_full_model/variables/*.data-00000-of-00001']},
+      package_data={'': extra_files},
+      # package_data={'whacc': ['whacc_data/*',
+      #                         'whacc/whacc_data/feature_data/*',
+      #                         'whacc/whacc_data/feature_data/*.pkl',
+      #                         'whacc/whacc_data/*.png',
+      #                         'whacc_data/final_model/final_resnet50V2_full_model/variables/*',
+      #                         'whacc_data/final_model/final_resnet50V2_full_model/assets/*',
+      #                         'whacc_data/final_model/final_resnet50V2_full_model/*',
+      #                         'whacc_data/final_model/*',
+      #                         'whacc/whacc_data/final_model/final_resnet50V2_full_model/*pb',
+      #                         'whacc/whacc_data/final_model/final_resnet50V2_full_model/variables/*.index',
+      #                         'whacc/whacc_data/final_model/final_resnet50V2_full_model/variables/*.data-00000-of-00001']},
       include_package_data=True
       )
 
